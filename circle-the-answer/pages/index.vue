d8dc8aa94672ce62471e8035d3eb3083641b2dda
Camille Sta Romana <staromanacamille@gmail.com>
2023-04-12 03:07:25 +0800

    test

--- circle-the-answer/pages/index.vue
+++ circle-the-answer/pages/index.vue
@@ -8,7 +8,7 @@
       @getPenColor="color"
       @getPenThickness="thickness"
     />
-
+    ////////// username /////////
     <enter-username  
       @back="backEnterUsername" 
       @next="nextEnterUsername" 
